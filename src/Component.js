029be6e356bb1db7421c49f6e958b3a5dc023512
PrashanAndradi <134471400+Prxshxn@users.noreply.github.com>
2024-10-17 22:32:36 +0530

    added edit and save button

--- src/Component.js
+++ src/Component.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 
+function generateId() {
+  return Math.floor(Math.random() * 1000000).toString();
+}
+
 export const Component = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
   const addTask = () => {
     if (newTask.trim() !== "") {
-      setTasks([...tasks, { text: newTask, completed: false }]);
+      setTasks([
+        ...tasks,
+        { id: generateId(), text: newTask, completed: false },
+      ]);
       setNewTask("");
     }
   };
@@ -18,10 +26,30 @@ export const Component = () => {
       )
     );
   };
+
   const removeTask = (taskId) => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
+  const handleEditTask = (taskId) => {
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    if (taskToEdit) {
+      setEditingTaskId(taskId);
+      setNewTask(taskToEdit.text);
+    }
+  };
+
+  const handleSaveEdit = () => {
+    if (newTask.trim() !== "") {
+      setTasks(
+        tasks.map((task) =>
+          task.id === editingTaskId ? { ...task, text: newTask } : task
+        )
+      );
+      setEditingTaskId(null);
+      setNewTask("");
+    }
+  };
 
   return (
     <div>
@@ -33,18 +61,31 @@ export const Component = () => {
           onChange={(e) => setNewTask(e.target.value)}
         />
         <button onClick={addTask}>Add</button>
-        
       </div>
 
       <ul className="task-list">
         {tasks.map((task) => (
           <li key={task.id} className={task.completed ? "completed" : ""}>
-            <span onClick={() => toggleTask(task.id)}>{task.text}</span>
-            <button onClick={() => removeTask(task.id)}>Remove</button> 
+            {editingTaskId === task.id ? (
+              <>
+                <input
+                  type="text"
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                />
+              </>
+              
+            ) : (
+              <span onClick={() => toggleTask(task.id)}>{task.text}</span>
+            )}
+            <button onClick={() => removeTask(task.id)}>Remove</button>
+            <button onClick={() => handleEditTask(task.id)}>Edit</button>
+            <button onClick={handleSaveEdit}>Save</button>
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
 export default Component;
